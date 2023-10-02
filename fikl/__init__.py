57d6bddf713bae5acc29dcc6a3e9cc043e3aa990
Robert Cofield <ozymandium@protonmail.com>
2023-10-01 20:21:29 -0500

    refactor to use fewer variables and more dataframes slightly improved, but more refactoring is needed.

--- fikl/__init__.py
+++ fikl/__init__.py
@@ -8,80 +8,65 @@ import logging
 import pprint
 
 
-MinMax = namedtuple("MinMax", ["min", "max"])
+def ensure_type(obj, t):
+    """Ensure that an object is of a certain type"""
+    if type(obj) is not t:
+        raise TypeError("object {} is not of type {}".format(obj, t))
 
 
-class Decision:
+class MinMax(namedtuple("MinMax", ["min", "max"])):
+    """A simple class to hold a min and max value, both of which are ints"""
 
-    ALL_KEY = "All"
+    def __new__(cls, min: int, max: int):
+        if min > max:
+            raise ValueError("min {} must be <= max {}".format(min, max))
+        ensure_type(min, int)
+        ensure_type(max, int)
+        return super().__new__(cls, min, max)
 
-    def __init__(
-        self,
-        choices: list[str],
-        scores: dict[str, dict[str, float]],
-        factors: list[str],
-        raw_weights: np.array,
-        score_range: MinMax,
-        metrics: dict[str, list[str]] = None,
-    ):
+    def __str__(self):
+        return "({}, {})".format(self.min, self.max)
+
+    def normalize(self, score: int) -> float:
         """
         Parameters
         ----------
-        choices : list[str]
-            names of the rows in the `choices` matrix. each row is a different choice.
-        scores : np.array
-            2d array. each row is a choice, each column is a value.
-        factors : list[str]
-            names of the columns in the `choices1` matrix. each column is a different value.
-        weights : np.array
-            1d array with a weighting for each value
-        score_range : MinMax
-            the minimum and maximum possible scores. if none provided, it will be inferred from
-            `scores`.
-        metrics : dict[str, list[str]]
-            different types of metrics. each key is a metric name, each value is a list of factors
-            that are included in that metric. an "All" metric is automatically added which includes
-            all factors
+        score : int
+            the score to normalize
+
+        Returns
+        -------
+        float
+            the normalized score
         """
-        self.logger = logging.getLogger()
+        return float(score - self.min) / float(self.range())
 
-        assert set(choices) == set(scores.keys())
-        # assert len(factors) == scores.shape[1]
-        assert len(factors) == len(raw_weights)
-        for _, included_factors in metrics.items():
-            for factor in included_factors:
-                assert factor in factors
+    def range(self) -> int:
+        """
+        Returns
+        -------
+        int
+            the range between the min and max
+        """
+        return self.max - self.min
 
-        # assert that all scores are ints
-        assert type(score_range.min) is int
-        assert type(score_range.max) is int
-        for choice in scores:
-            for factor in scores[choice]:
-                assert type(scores[choice][factor]) is np.int64
 
-        self.choices = choices
-        self.scores = scores
-        self.factors = factors
-        self.score_range = score_range
+class Decision:
+    """
+    Members
+    -------
+    logger : logging.Logger
+        logger for this class
+    scores : pd.DataFrame
+        the ranking matrix. the index is the choice name, the columns are the factors
+    score_range : MinMax
+        the minimum and maximum possible scores.
+    weights : pd.DataFrame
+        the weights for each factor for each metric. the index is the metric name, the columns are
+        the factors. the "All" metric is automatically added which includes all factors
+    """
 
-        # automatically add a metric with all factors
-        self.metrics = {self.ALL_KEY: factors}
-        if metrics is not None:
-            for name, included_factors in metrics.items():
-                self.metrics[name] = included_factors
-
-        # weights are normalized for each metric
-        self.weights = {}
-        for metric in self.metrics:
-            # weights are in same order as factors
-            weights = np.array(
-                [raw_weights[self.factors.index(factor)] for factor in self.metrics[metric]]
-            )
-            self.weights[metric] = weights / np.sum(weights)
-        self.logger.info("Weights:\n{}".format(pprint.pformat(self.weights)))
-
-        self.results = self._get_results()
-        self.logger.debug("Results:\n{}".format(pprint.pformat(self.results)))
+    ALL_KEY = "All"
 
     @staticmethod
     def read(config_path: str, data_path: str):
@@ -101,77 +86,149 @@ class Decision:
         -------
         Decision
         """
+        # read the config yaml
         with open(config_path, "r") as f:
             config = yaml.load(f, Loader=yaml.FullLoader)
-        # read the ranking matrix from the csv as a dataframe
-        data = pd.read_csv(data_path)
 
-        # ensure that data has a "name" column and all other colums are factors
-        if set(data.columns) != set(config["weights"].keys()).union({"name"}):
+        # read the ranking matrix from the csv as a dataframe
+        # the index is the choice name, the columns are the factors
+        scores = pd.read_csv(data_path, index_col="choice")
+
+        # ensure that scores has a "choice" column and all other colums are factors
+        if set(scores.columns) != set(config["factors"].keys()):
             raise ValueError(
-                "data columns {} do not match config weights {}".format(
-                    set(data.columns), set(config["weights"].keys())
+                "score columns {} do not match config factors {}".format(
+                    set(scores.columns), set(config["factors"].keys())
                 )
             )
 
-        choices = data["name"].tolist()
-        factors = [k for k, _ in sorted(config["weights"].items(), key=lambda item: item[1])]
-        scores = {
-            choice: {factor: data[factor][data["name"] == choice].iloc[0] for factor in factors}
-            for choice in choices
-        }
-        weights = [config["weights"][factor] for factor in factors]
-        metrics = config["metrics"]
-        score_range = MinMax(**config["score_range"])
+        # weight should be a DataFrame where the columns are factors and the index is the metric.
+        # columns should be the same as the factors in the scores. Initialize with all zeros.
+        weights = pd.DataFrame(
+            0, columns=scores.columns, index=[Decision.ALL_KEY] + list(config["metrics"].keys())
+        )
+        # the "All" metric should have all factors populated with values from the config
+        for factor in config["factors"]:
+            weights.loc[Decision.ALL_KEY, factor] = config["factors"][factor]["weight"]
+        # for each metric, copy all the weights for included factors from the "All" weights
+        for metric in config["metrics"]:
+            for factor in config["metrics"][metric]:
+                weights.loc[metric, factor] = weights.loc[Decision.ALL_KEY, factor]
+        # normalize the weights for each metric (along each row)
+        weights = weights.div(weights.sum(axis=1), axis=0)
+
+        score_range = MinMax(**config["score"]["range"])
 
         return Decision(
-            choices=choices,
             scores=scores,
-            factors=factors,
             raw_weights=weights,
-            metrics=metrics,
             score_range=score_range,
         )
 
-    def _aggregate(self, metric, choice: str = None, fixed_score: float = None):
-        factors = self.metrics[metric]
-        # scores in order of factors
-        if fixed_score is not None:
-            assert choice is None  # just a check
-            scores = fixed_score * np.ones((len(factors),))
-        else:
-            assert choice is not None
-            scores = np.array([self.scores[choice][factor] for factor in factors])
-        return self.weights[metric].dot(scores)
+    def __init__(
+        self,
+        scores: pd.DataFrame,
+        raw_weights: np.array,
+        score_range: MinMax,
+    ):
+        """
+        Parameters
+        ----------
+        scores : pd.DataFrame
+            the ranking matrix. the index is the choice name, the columns are the factors
+        raw_weights : np.array
+            1d array with a weighting for each value, in the same order as the factors in the
+            scores. these weights are not normalized, that is done in the ctor.
+        score_range : MinMax
+            the minimum and maximum possible scores. if none provided, it will be inferred from
+            `scores`.
+        """
+        self.logger = logging.getLogger()
+
+        self.scores = scores.copy()
+        self.logger.info("Scores:\n{}".format(pprint.pformat(self.scores)))
+
+        self.score_range = score_range
+        self.logger.info("Score range: {}".format(self.score_range))
+
+        # weights are normalized for each metric
+        self.weights = raw_weights.copy()
+        self.logger.info("Weights:\n{}".format(pprint.pformat(self.weights)))
+
+        # check and make sure that the columns are shared between scores and weights
+        if set(self.scores.columns) != set(self.weights.columns):
+            raise ValueError(
+                "score columns {} do not match weight columns {}".format(
+                    set(self.scores.columns), set(self.weights.columns)
+                )
+            )
+
+    def factors(self) -> list[str]:
+        """
+        Returns
+        -------
+        list[str]
+            list of factor names
+        """
+        return list(self.scores.columns)
+
+    def choices(self) -> list[str]:
+        """
+        Returns
+        -------
+        list[str]
+            list of choice names
+        """
+        return list(self.scores.index)
+
+    def metrics(self) -> list[str]:
+        """
+        Returns
+        -------
+        list[str]
+            list of metric names
+        """
+        return list(self.weights.index)
+
+    def _aggregate(self, metric: str, choice: str):
+        """
+        compute the aggregate score for a choice for a metric. this should be automatically
+        normalized to [0, 1]. compute as the dot product of the weights for the metric and the
+        scores for the choice.
+
+        Parameters
+        ----------
+        metric : str
+            the metric to compute the aggregate score for
+        choice : str
+            the choice to compute the aggregate score for
+
+        Returns
+        -------
+        float
+            the aggregate score for the choice for the metric
+        """
+        return np.dot(self.weights.loc[metric], self.scores.loc[choice])
 
     def _get_results(self):
         results = {}
-        for metric in self.metrics.keys():
+        for metric in self.metrics():
             results[metric] = {}
-            for choice in self.choices:
+            for choice in self.choices():
                 agg = self._aggregate(metric, choice)
-                results[metric][choice] = Decision._scalar_from_score(agg, self.score_range)
+                results[metric][choice] = self.score_range.normalize(agg)
         return results
 
-    def _get_scores_df(self) -> pd.DataFrame:
-        arr = np.empty((len(self.choices), len(self.factors)), dtype=np.int32)
-        for row, choice in enumerate(self.choices):
-            for col, factor in enumerate(self.factors):
-                arr[row, col] = self.scores[choice][factor]
-        return pd.DataFrame(arr, columns=self.factors, index=self.choices)
-
     def _get_results_df(self) -> pd.DataFrame:
-        metrics = list(self.metrics.keys())
-        arr = np.empty((len(self.choices), len(metrics)))
-        for row, choice in enumerate(self.choices):
+        results = self._get_results()
+        self.logger.debug("Results:\n{}".format(pprint.pformat(results)))
+        metrics = list(self.metrics())
+        arr = np.empty((len(self.choices()), len(metrics)))
+        for row, choice in enumerate(self.choices()):
             for col, metric in enumerate(metrics):
-                arr[row, col] = self.results[metric][choice]
-        df = pd.DataFrame(arr, columns=metrics, index=self.choices)
+                arr[row, col] = results[metric][choice]
+        df = pd.DataFrame(arr, columns=metrics, index=self.choices())
         return df
-
-    @staticmethod
-    def _scalar_from_score(score, score_range):
-        return (score - score_range.min) / (score_range.max - score_range.min)
 
     def to_html(self, path: str = None):
         """
@@ -180,7 +237,7 @@ class Decision:
         path : str
             File path where html should be written
         """
-        table = pd.concat((self._get_scores_df(), self._get_results_df()), axis=1)
+        table = pd.concat((self.scores, self._get_results_df()), axis=1)
         self.logger.debug("table:\n{}".format(table))
         table = table.sort_values(self.ALL_KEY)
 
@@ -190,7 +247,7 @@ class Decision:
         styler = styler.background_gradient(
             axis="index",
             cmap=sns.color_palette("blend:darkred,green", as_cmap=True),
-            subset=self.factors,
+            subset=self.scores.columns.to_list(),
             vmin=self.score_range.min,
             vmax=self.score_range.max,
         )
@@ -198,11 +255,11 @@ class Decision:
         styler = styler.background_gradient(
             axis="index",
             cmap=sns.color_palette("blend:darkred,green", as_cmap=True),
-            subset=list(self.metrics.keys()),
+            subset=list(self.metrics()),
             vmin=0,
             vmax=1,
         )
-        styler = styler.format({metric: "{0:.0%}" for metric in self.metrics.keys()})
+        styler = styler.format({metric: "{0:.0%}" for metric in self.metrics()})
         styler = styler.set_table_styles(
             [{"selector": "th", "props": [("font-family", "Courier"), ("font-size", "11px")]}]
         )
