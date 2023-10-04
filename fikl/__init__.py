c1861309730abe7dd4614aa67b1af02a8739456f
Robert Cofield <ozymandium@protonmail.com>
2023-10-04 00:22:53 -0500

    overhaul to scorers and using floats from 0 to 1 instead of stars

--- fikl/__init__.py
+++ fikl/__init__.py
@@ -6,7 +6,7 @@ import seaborn as sns
 from collections import namedtuple
 import logging
 import pprint
-from typing import Optional, Any
+from typing import Optional, Any, Dict, List, Callable
 
 
 def ensure_type(obj, t):
@@ -15,65 +15,167 @@ def ensure_type(obj, t):
         raise TypeError("object {} is not of type {}".format(obj, t))
 
 
-def scorer_int(val: Any, config: dict) -> int:
-    """scoring function that simply returns the value as an int"""
-    return int(val)
-
-
-def scorer_bin(val: Any, config: dict) -> int:
+class StarScorer:
     """
-    scoring function that bins the value based on the config. when this is used, the config should
-    be a list of dicts, each with a "min", "max", and "val" key. the value is compared to the min
-    and max, and if it is in that range, the val is returned. if the value is not in any of the
-    ranges, a ValueError is raised.
+    scorer that accepts input as ints on a fixed scale, such as the 5 start scale, where
+    1 is the lowest and 5 is the highest.
     """
-    for bin in config:
-        if bin["min"] <= val < bin["max"]:
-            return bin["val"]
-    raise ValueError("value {} is not in any of the bins".format(val))
 
+    # how user requests that scoring be done using this scorer
+    CODE = "star"
+    # required type of input
+    DTYPE = int
 
-SCORERS = {
-    "int": scorer_int,
-    "bin": scorer_bin,
-}
-
-
-class MinMax(namedtuple("MinMax", ["min", "max"])):
-    """A simple class to hold a min and max value, both of which are ints"""
-
-    def __new__(cls, min: int, max: int):
-        if min > max:
-            raise ValueError("min {} must be <= max {}".format(min, max))
+    def __init__(self, min: int, max: int):
         ensure_type(min, int)
         ensure_type(max, int)
-        return super().__new__(cls, min, max)
+        if min >= max:
+            raise ValueError("min {} must be < max {}".format(min, max))
+        self.min = min
+        self.max = max
+        self.range = max - min
 
-    def __str__(self):
-        return "({}, {})".format(self.min, self.max)
-
-    def normalize(self, score: int) -> float:
+    def __call__(self, col: pd.Series) -> pd.Series:
         """
         Parameters
         ----------
-        score : int
-            the score to normalize
+        col : pd.Series
+            the column to score
 
         Returns
         -------
-        float
-            the normalized score
+        pd.Series
+            the scored column, with values between 0 and 1
         """
-        return float(score - self.min) / float(self.range())
+        # make sure all values are between the min and max
+        if not (col >= self.min).all():
+            raise ValueError(f"all values in column must be >= {self.min}, but got\n{col}")
+        if not (col <= self.max).all():
+            raise ValueError(f"all values in column must be <= {self.max}, but got\n{col}")
+        # make sure all values are ints
+        if not col.dtype == int:
+            raise ValueError(f"all values in column must be ints but col dtype is {col.dtype}")
+        # compute the return
+        ret = (col - self.min) / self.range
+        # make sure all values lie between 0 and 1
+        if not (ret >= 0).all():
+            raise ValueError(f"all values in column must be >= 0, but got\n{ret}")
+        if not (ret <= 1).all():
+            raise ValueError(f"all values in column must be <= 1, but got\n{ret}")
+        return ret
 
-    def range(self) -> int:
+
+class BucketScorer:
+    """User sets bins for lumping input values into groups that all get that same score."""
+
+    # how user requests that scoring be done using this scorer
+    CODE = "bucket"
+    # required type of input
+    DTYPE = float
+
+    class Bucket(namedtuple("Bucket", ["min", "max", "val"])):
+        """A bucket is a range of values that all get the same score."""
+
+        def __new__(cls, min: float, max: float, val: float):
+            # if inputs are not floats, cast them to floats, but log a warning
+            if not isinstance(min, float):
+                logging.warning(f"min {min} is not a float, casting to float")
+                min = float(min)
+            if not isinstance(max, float):
+                logging.warning(f"max {max} is not a float, casting to float")
+                max = float(max)
+            if not isinstance(val, float):
+                logging.warning(f"val {val} is not a float, casting to float")
+                val = float(val)
+            # validate inputs
+            if min >= max:
+                raise ValueError(f"min {min} must be < max {max}")
+            if val < 0 or val > 1:
+                raise ValueError(f"val {val} must be between 0 and 1")
+            return super().__new__(cls, min, max, val)
+
+    def __init__(self, buckets: List[Dict[str, float]]):
         """
+        Parameters
+        ----------
+        buckets : dict
+            each bucket is a dict with keys "min", "max", and "val". min and max are the range of
+            values that get the score val. min and max must be the same type. val must be a float
+            between 0 and 1.
+        """
+        ensure_type(buckets, list)
+        # store buckets in order of increasing min. Allow the bucket ctor to do the validation.
+        self.buckets: List[Bucket] = sorted(
+            [self.Bucket(**b) for b in buckets], key=lambda b: b.min
+        )
+        # ensure that the buckets are contiguous
+        for i, bucket in enumerate(self.buckets[:-1]):
+            if bucket.max != self.buckets[i + 1].min:
+                raise ValueError(
+                    "buckets must be contiguous, but got buckets {} and {}".format(
+                        bucket, self.buckets[i + 1]
+                    )
+                )
+        # ensure the type of the min and max are the same for all buckets
+        if not all([type(bucket.min) is type(self.buckets[0].min) for bucket in self.buckets]):
+            raise TypeError(
+                "all buckets must have the same type for min and max, but got {}".format(
+                    [type(bucket.min) for bucket in self.buckets]
+                )
+            )
+
+    def __call__(self, col: pd.Series) -> pd.Series:
+        """
+        Parameters
+        ----------
+        col : pd.Series
+            the column to score
+
         Returns
         -------
-        int
-            the range between the min and max
+        pd.Series
+            the scored column, with values between 0 and 1
         """
-        return self.max - self.min
+        # make sure all values are between the min and max
+        if not (col >= self.buckets[0].min).all():
+            raise ValueError(
+                f"all values in column must be >= {self.buckets[0].min}, but got\n{col}"
+            )
+        if not (col <= self.buckets[-1].max).all():
+            raise ValueError(
+                f"all values in column must be <= {self.buckets[-1].max}, but got\n{col}"
+            )
+        # make sure all values are same type as bucket min and max
+        if not col.dtype == type(self.buckets[0].min):
+            raise ValueError(
+                f"all values in column must be same type as bucket min {self.buckets[0].min} but col dtype is {col.dtype}"
+            )
+        # make sure all inputs are between the min and max of some bucket
+        if not (col >= self.buckets[0].min).any():
+            raise ValueError(
+                f"all values in column must be >= {self.buckets[0].min}, but got\n{col}"
+            )
+        if not (col <= self.buckets[-1].max).any():
+            raise ValueError(
+                f"all values in column must be <= {self.buckets[-1].max}, but got\n{col}"
+            )
+        # compute the return
+        ret = np.zeros_like(col, dtype=float)
+        for bucket in self.buckets:
+            ret[(col >= bucket.min) & (col < bucket.max)] = bucket.val
+        # make sure all values lie between 0 and 1
+        if not (ret >= 0).all():
+            raise ValueError(f"all values in column must be >= 0, but got\n{ret}")
+        if not (ret <= 1).all():
+            raise ValueError(f"all values in column must be <= 1, but got\n{ret}")
+        return ret
+
+
+SCORERS = {
+    StarScorer,
+    BucketScorer,
+}
+SCORERS_LOOKUP = {scorer.CODE: scorer for scorer in SCORERS}
 
 
 class Decision:
@@ -82,8 +184,11 @@ class Decision:
     -------
     logger : logging.Logger
         logger for this class
+    raw : pd.DataFrame
+        the raw user input matrix. the index is the choice name, the columns are the factors
     scores : pd.DataFrame
-        the ranking matrix. the index is the choice name, the columns are the factors
+        the ranking matrix. the index is the choice name, the columns are the factors. values are
+        floats between 0 and 1.
     score_range : MinMax
         the minimum and maximum possible scores.
     weights : pd.DataFrame
@@ -93,10 +198,6 @@ class Decision:
 
     def __init__(self, config_path: str, data_path: str):
         """
-        TODO: get away from having ctor and parsing in different formats, and make it so that the
-        ctor args are exactly the same as the yaml content. intermediate variable creation should be
-        moved to ctor
-
         Parameters
         ----------
         config_path : str
@@ -116,28 +217,35 @@ class Decision:
 
         # read the ranking matrix from the csv as a dataframe
         # the index is the choice name, the columns are the factors
-        raw_scores = pd.read_csv(data_path, index_col="choice")
-        self.logger.debug("raw scores:\n{}".format(raw_scores))
+        self.raw = pd.read_csv(data_path, index_col="choice")
+        # allow the user to input executable code in the csv. eval it here.
+        self.raw = self.raw.applymap(lambda x: eval(x) if isinstance(x, str) else x)
+        self.logger.debug("raw scores:\n{}".format(self.raw))
+
         # determine which scorer to use for each factor
         # a scorer takes in the value from raw_scores and the config for that factor, and returns
         # an int score that is inside of the score range described by MinMax
-        scorer_config = {factor: c["config"] for factor, c in config["factors"].items()}
-        scorer = {factor: SCORERS[c["type"]] for factor, c in config["factors"].items()}
-        self.logger.debug("scorer: {}".format(scorer))
-        # for each column in raw_scores, apply the scorer that corresponds to that factor, and
-        # pass in the scorer config for that factor to the scorer.
-        self.scores = raw_scores.apply(
-            lambda col: col.apply(lambda val: scorer[col.name](val, scorer_config[col.name]))
-        )
-        self.logger.info("Scores:\n{}".format(pprint.pformat(self.scores)))
+        scorers: Dict[str, Callable] = {}
+        for factor, cfg in config["factors"].items():
+            scorer_t = SCORERS_LOOKUP[cfg["type"]]
+            scorer_config = cfg["config"]
+            scorers[factor] = scorer_t(**scorer_config)
+        self.logger.debug(f"scorers: {scorers}")
 
-        # # ensure that scores has a "choice" column and all other colums are factors
-        # if set(self.scores.columns) != set(config["factors"].keys()):
-        #     raise ValueError(
-        #         "score columns {} do not match config factors {}".format(
-        #             set(self.scores.columns), set(config["factors"].keys())
-        #         )
-        #     )
+        # each scorer requires a certain dtype for the input. iterate over each factor/column and
+        # make sure that the dtype is correct. if not, try to cast it to the correct type and log a
+        # warning.
+        for factor, scorer in scorers.items():
+            dtype = scorer.DTYPE
+            if not self.raw[factor].dtype == dtype:
+                logging.warning(
+                    f"factor {factor} has dtype {self.raw[factor].dtype} but scorer {scorer} requires dtype {dtype}, casting to {dtype}"
+                )
+                self.raw[factor] = self.raw[factor].astype(dtype)
+
+        # for each column in self.raw, apply the scorer that corresponds to that factor
+        self.scores = self.raw.apply(lambda col: scorers[col.name](col), axis=0)
+        self.logger.info("Scores:\n{}".format(pprint.pformat(self.scores)))
 
         # weight should be a DataFrame where the columns are factors and the index is the metric.
         # columns should be the same as the factors in the scores. Initialize with all zeros.
@@ -153,10 +261,6 @@ class Decision:
         # normalize the weights for each metric (along each row)
         self.weights = self.weights.div(self.weights.sum(axis=1), axis=0)
         self.logger.info("Weights:\n{}".format(pprint.pformat(self.weights)))
-
-        score_range = MinMax(**config["score"])
-        self.score_range = score_range
-        self.logger.debug("Score range: {}".format(self.score_range))
 
         # check and make sure that the columns are shared between scores and weights
         if set(self.scores.columns) != set(self.weights.columns):
@@ -193,26 +297,6 @@ class Decision:
         """
         return list(self.weights.index)
 
-    def _aggregate(self, metric: str, choice: str):
-        """
-        compute the aggregate score for a choice for a metric. this should be automatically
-        normalized to [0, 1]. compute as the dot product of the weights for the metric and the
-        scores for the choice.
-
-        Parameters
-        ----------
-        metric : str
-            the metric to compute the aggregate score for
-        choice : str
-            the choice to compute the aggregate score for
-
-        Returns
-        -------
-        float
-            the aggregate score for the choice for the metric
-        """
-        return np.dot(self.weights.loc[metric], self.scores.loc[choice])
-
     def _get_results(self) -> pd.DataFrame:
         results = pd.DataFrame(
             index=self.choices(),
@@ -221,8 +305,8 @@ class Decision:
         )
         for choice in self.choices():
             for metric in self.metrics():
-                results.loc[choice, metric] = self.score_range.normalize(
-                    self._aggregate(metric, choice)
+                results.loc[choice, metric] = np.dot(
+                    self.weights.loc[metric], self.scores.loc[choice]
                 )
         return results
 
@@ -248,19 +332,19 @@ class Decision:
         styler = styler.background_gradient(
             axis="index",
             cmap=sns.color_palette("blend:darkred,green", as_cmap=True),
-            subset=self.scores.columns.to_list(),
-            vmin=self.score_range.min,
-            vmax=self.score_range.max,
+            vmin=0.0,
+            vmax=1.0,
         )
-        # background gradient for the results is separate, those are percentages
-        styler = styler.background_gradient(
-            axis="index",
-            cmap=sns.color_palette("blend:darkred,green", as_cmap=True),
-            subset=list(self.metrics()),
-            vmin=0,
-            vmax=1,
-        )
-        styler = styler.format({metric: "{0:.0%}" for metric in self.metrics()})
+        # # background gradient for the results is separate, those are percentages
+        # styler = styler.background_gradient(
+        #     axis="index",
+        #     cmap=sns.color_palette("blend:darkred,green", as_cmap=True),
+        #     subset=list(self.metrics()),
+        #     vmin=0,
+        #     vmax=1,
+        # )
+        # styler = styler.format({metric: "{0:.0%}" for metric in self.metrics()})
+        styler = styler.format("{0:.0%}")
         styler = styler.set_table_styles(
             [{"selector": "th", "props": [("font-family", "Courier"), ("font-size", "11px")]}]
         )
