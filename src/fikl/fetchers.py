c51e181be1ebd676b2cc82cb560a64167b4101e2
Robert Cofield <ozymandium@protonmail.com>
2023-11-30 20:40:25 -0600

    wip

--- src/fikl/fetchers.py
+++ src/fikl/fetchers.py
@@ -12,8 +12,12 @@ All fetchers must implement:
 """
 from typing import List
 import os
+import pprint
+import logging
+import requests
 
 import pandas as pd
+import geocoder
 
 
 class ObesityFetcher:
@@ -77,6 +81,13 @@ class CountyElectionMargin:
         os.path.dirname(__file__), "data", "countypres_2000-2020.csv"
     )
 
+    def __init__(self): 
+        self.df = pd.read_csv(self.SOURCE_FILE)
+        # filter out non-2020 data
+        self.df = self.df[self.df["year"] == 2020]
+        # filter out rows where party is not DEMOCRAT or REPUBLICAN
+        self.df = self.df[self.df["party"].isin(["DEMOCRAT", "REPUBLICAN"])]
+
     def fetch(self, choices: List[str]) -> pd.Series:
         """
         Fetches the margin of victory for the 2020 presidential election for a county. Looks up the
@@ -94,11 +105,54 @@ class CountyElectionMargin:
             Series with the same index as the list of choices. Values are the victory margin for the
             winning candidate for the choice, as a percentage of the total votes cast.
         """
+        logger = logging.getLogger(__name__)
+        ret = []
+
+        session = requests.Session()
+
+        for choice in choices:
+            # use geocoder to get the county name from the choice
+            g = geocoder.osm(choice, session=session)
+            logging.debug(f"geocoder result for {choice}:\n{pprint.pformat(g.json)}")
+
+            # `county` will be formatted as "Suffolk County". need to convert it to "SUFFOLK"
+            county = g.json["raw"]["address"]["county"]
+            county = county.replace(" County", "").upper()
+            # state abbreviation is in the address as "US-MA"
+            # FIXME: can uppercase the state in the geocoder query and remove this filter instead of
+            #        replacing "US-" with ""
+            state = g.json["raw"]["address"]["ISO3166-2-lvl4"]
+            state = state.replace("US-", "")
+            # filter the data to only the county and state we want
+            df = self.df[(self.df["county_name"] == county) & (self.df["state_po"] == state)]
+
+            # ensure totalvotes is the same for both rows
+            if df["totalvotes"].nunique() != 1:
+                raise ValueError("totalvotes not the same for both parties")
+            
+            # the margin is the percent of votes for the winning party minus the percent of votes
+            # for the losing party. don't need to determine the winner, just take the difference
+            # between the two percentages and return the absolute value.
+            # some states have separate results for different types of voting, e.g. mail-in vs
+            # in-person. we don't care about that, so sum all the votes for each party.
+            # d_votes = float(df[df["party"] == "DEMOCRAT"]["candidatevotes"].iloc[0])
+            # r_votes = float(df[df["party"] == "REPUBLICAN"]["candidatevotes"].iloc[0])
+            d_votes = float(df[df["party"] == "DEMOCRAT"]["candidatevotes"].sum())
+            r_votes = float(df[df["party"] == "REPUBLICAN"]["candidatevotes"].sum())
+            total_votes = float(df["totalvotes"].iloc[0])
+            margin = abs((d_votes - r_votes) / total_votes) * 100.
+            ret.append(margin)
+
+        session.close()
+
+        return pd.Series(ret, index=choices)
+
 
 
 LOOKUP = {
     fetcher.CODE: fetcher
     for fetcher in [
         ObesityFetcher,
+        CountyElectionMargin,
     ]
 }
