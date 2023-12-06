01ccfa56343dbf87dc30b380ec20391d901beb88
Robert Cofield <ozymandium@protonmail.com>
2023-12-06 13:45:58 -0600

    zori working

--- src/fikl/fetchers.py
+++ src/fikl/fetchers.py
@@ -353,15 +353,18 @@ class ZoriFetcher:
 
     Zillow Observed Rent Index (ZORI): A smoothed measure of the typical observed market rate rent
     across a given region. ZORI is a repeat-rent index that is weighted to the rental housing stock
-    to ensure representativeness across the entire market, not just those homes currently listed 
-    for-rent. The index is dollar-denominated by computing the mean of listed rents that fall into 
-    the 40th to 60th percentile range for all homes and apartments in a given region, which is once 
+    to ensure representativeness across the entire market, not just those homes currently listed
+    for-rent. The index is dollar-denominated by computing the mean of listed rents that fall into
+    the 40th to 60th percentile range for all homes and apartments in a given region, which is once
     again weighted to reflect the rental housing stock. Details available in ZORI methodology.
 
     Note that this is for all rentals, not just apartments, or certain sizes of apartments.
     """
+
     CODE = "ZORI"
-    SOURCE_FILE = os.path.join(os.path.dirname(__file__), "data", "City_zori_uc_sfrcondomfr_sm_sa_month.csv")
+    SOURCE_FILE = os.path.join(
+        os.path.dirname(__file__), "data", "City_zori_uc_sfrcondomfr_sm_sa_month.csv"
+    )
 
     def __init__(self):
         self.df = pd.read_csv(self.SOURCE_FILE)
